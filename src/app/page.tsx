8eb217368f8fa63fb2db91c279de78b4f49c7b52
Yoshie Maeda <cassisism@gmail.com>
2024-09-08 17:21:10 +0900

    change frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,106 @@
 "use client";
 
 import Image from "next/image";
-import { ConnectButton } from "thirdweb/react";
-import thirdwebIcon from "@public/thirdweb.svg";
+import {
+  ConnectButton,
+  MediaRenderer,
+  TransactionButton,
+  useActiveAccount,
+  useReadContract,
+} from "thirdweb/react";
+
 import { client } from "./client";
+import { defineChain, getContract, toEther } from "thirdweb";
+import { sepolia } from "thirdweb/chains";
+import { getContractMetadata } from "thirdweb/extensions/common";
+import {
+  claimTo,
+  getActiveClaimCondition,
+  getTotalClaimedSupply,
+  nextTokenIdToMint,
+} from "thirdweb/extensions/erc721";
+import { useState } from "react";
 
 export default function Home() {
+  const account = useActiveAccount();
+
+  const chain = defineChain(sepolia);
+
+  const [quantity, setQuantity] = useState(1);
+
+  const contract = getContract({
+    client: client,
+    chain: chain,
+    address: "0x60058dc53f89B7e61fD80958DE6Bb1572E7a35b2",
+  });
+
+  const { data: contractMetadata, isLoading: isContractMetadataLaoding } =
+    useReadContract(getContractMetadata, { contract: contract });
+
+  const { data: claimedSupply, isLoading: isClaimedSupplyLoading } =
+    useReadContract(getTotalClaimedSupply, { contract: contract });
+
+  const { data: totalNFTSupply, isLoading: isTotalSupplyLoading } =
+    useReadContract(nextTokenIdToMint, { contract: contract });
+
+  const { data: claimCondition } = useReadContract(getActiveClaimCondition, {
+    contract: contract,
+  });
+
+  const getPrice = (quantity: number) => {
+    const total =
+      quantity * parseInt(claimCondition?.pricePerToken.toString() || "0");
+    return toEther(BigInt(total));
+  };
+
   return (
     <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-      <div className="py-20">
+      <div className="py-20 text-center">
         <Header />
-
-        <div className="flex justify-center mb-20">
-          <ConnectButton
-            client={client}
-            appMetadata={{
-              name: "Example App",
-              url: "https://example.com",
+        <ConnectButton client={client} chain={chain} />
+        <div className="flex flex-col items-center mt-4">
+          {isContractMetadataLaoding ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              <MediaRenderer
+                client={client}
+                src={contractMetadata?.image}
+                className="rounded-xl"
+              />
+              <h2 className="text-2xl font-semibold mt-4">
+                {contractMetadata?.name}
+              </h2>
+              <p className="text-lg mt-2">{contractMetadata?.description}</p>
+            </>
+          )}
+          {isClaimedSupplyLoading || isTotalSupplyLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <p className="text-lg mt-2 font-bold">
+              Total Supply: {claimedSupply?.toString()}/
+              {totalNFTSupply?.toString()}
+            </p>
+          )}
+          <div className="flex flex-row items-center justify-center my-4">
+            <p>Free</p>
+          </div>
+          <TransactionButton
+            transaction={() =>
+              claimTo({
+                contract: contract,
+                to: account?.address || "",
+                quantity: BigInt(quantity),
+              })
+            }
+            onTransactionConfirmed={async () => {
+              alert("Partner prooven!");
+              setQuantity(1);
             }}
-          />
+          >
+            {`Claim Partnership (${getPrice(quantity)} ETH)`}
+          </TransactionButton>
         </div>
-
-        <ThirdwebResources />
       </div>
     </main>
   );
@@ -29,72 +108,10 @@ export default function Home() {
 
 function Header() {
   return (
-    <header className="flex flex-col items-center mb-20 md:mb-20">
-      <Image
-        src={thirdwebIcon}
-        alt=""
-        className="size-[150px] md:size-[150px]"
-        style={{
-          filter: "drop-shadow(0px 0px 24px #a726a9a8)",
-        }}
-      />
-
-      <h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
-        thirdweb SDK
-        <span className="text-zinc-300 inline-block mx-1"> + </span>
-        <span className="inline-block -skew-x-6 text-blue-500"> Next.js </span>
-      </h1>
-
-      <p className="text-zinc-300 text-base">
-        Read the{" "}
-        <code className="bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1">
-          README.md
-        </code>{" "}
-        file to get started.
-      </p>
+    <header className="flex flex-row items-center">
+      <h2 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
+        PartnerProof of ALICE
+      </h2>
     </header>
-  );
-}
-
-function ThirdwebResources() {
-  return (
-    <div className="grid gap-4 lg:grid-cols-3 justify-center">
-      <ArticleCard
-        title="thirdweb SDK Docs"
-        href="https://portal.thirdweb.com/typescript/v5"
-        description="thirdweb TypeScript SDK documentation"
-      />
-
-      <ArticleCard
-        title="Components and Hooks"
-        href="https://portal.thirdweb.com/typescript/v5/react"
-        description="Learn about the thirdweb React components and hooks in thirdweb SDK"
-      />
-
-      <ArticleCard
-        title="thirdweb Dashboard"
-        href="https://thirdweb.com/dashboard"
-        description="Deploy, configure, and manage your smart contracts from the dashboard."
-      />
-    </div>
-  );
-}
-
-function ArticleCard(props: {
-  title: string;
-  href: string;
-  description: string;
-}) {
-  return (
-    <a
-      href={props.href + "?utm_source=next-template"}
-      target="_blank"
-      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-    >
-      <article>
-        <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-        <p className="text-sm text-zinc-400">{props.description}</p>
-      </article>
-    </a>
   );
 }
